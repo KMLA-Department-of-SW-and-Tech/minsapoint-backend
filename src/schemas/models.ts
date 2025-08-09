95a6af3ced6e4a8d9caa75477f3571229932e75d
codeweb404 <codeweb.jcu@gmail.com>
2025-08-10 01:22:07 +0900

    created user schemas

--- src/schemas/models.ts
+++ src/schemas/models.ts
@@ -1,6 +1,30 @@
+export enum UserRole {
+  STUDENT = 'STUDENT',
+  ACCUSER = 'ACCUSER',
+  DEPT_OF_JUSTICE = 'DEPT_OF_JUSTICE',
+  ADMIN = 'ADMIN',
+}
+
+export interface BaseUser {
+  _id: string;
+  firebase_uid: string;
+  role: UserRole;
+  name: string;
+}
+
+export interface Student extends BaseUser {
+  role: UserRole.STUDENT;
+  studentNumber: number;
+  penaltyPoints: number;
+  rewardPoints: number;
+  totalPoints: number;
+  hasCourt: boolean;
+}
+
 export interface CorrectionLog {
   _id: string;
   correctedDocs: string;
   date: string;
   description: string;
 }
+
