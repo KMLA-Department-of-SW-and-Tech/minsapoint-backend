25436125164f8870de7bc0039e079da83b731c99
hyk03031 <yha525320@gmail.com>
2025-08-11 00:14:29 +0900

    allowed all origin(cors)

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import './config/firebaseConfig';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // MUST TODO: Set SOP for production
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
