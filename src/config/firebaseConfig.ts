b296c2f84b092d1c67e6f2253a3c9f8e2146a723
hyk03031 <yha525320@gmail.com>
2025-08-15 19:28:16 +0900

    added initializing code in  main.ts for .env

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,12 +1,11 @@
 import * as admin from 'firebase-admin';
 
-
 // initialize firebase admin sdk
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID, 
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   }),
 });
 const firebaseAdmin = admin;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config'
 import './config/firebaseConfig';
 
 async function bootstrap() {
