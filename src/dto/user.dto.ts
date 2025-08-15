0e5666ddbc5c280123420fa69f3a47d2382ff6fa
codeweb404 <codeweb.jcu@gmail.com>
2025-08-15 21:30:49 +0900

    created user dto

---
+++ src/dto/user.dto.ts
@@ -0,0 +1,83 @@
+import { UserRole } from '../schemas/models';
+import {
+  IsString,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsBoolean,
+  Min,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsOptional()
+  @IsNumber()
+  @Min(1000)
+  studentNumber?: number;
+
+  @IsString()
+  @MaxLength(100)
+  name: string;
+
+  @IsEnum(UserRole)
+  role: UserRole;
+
+  @IsOptional()
+  @IsNumber()
+  penaltyPoints?: number;
+
+  @IsOptional()
+  @IsNumber()
+  rewardPoints?: number;
+
+  @IsOptional()
+  @IsNumber()
+  totalPoints?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  hasCourt?: boolean;
+}
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsNumber()
+  @Min(1000)
+  studentNumber?: number;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  name?: string;
+
+  @IsOptional()
+  @IsEnum(UserRole)
+  role?: UserRole;
+
+  @IsOptional()
+  @IsNumber()
+  penaltyPoints?: number;
+
+  @IsOptional()
+  @IsNumber()
+  rewardPoints?: number;
+
+  @IsOptional()
+  @IsNumber()
+  totalPoints?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  hasCourt?: boolean;
+}
+
+export class UserResponseDto {
+  _id: string;
+  role: UserRole;
+  name: string;
+  studentNumber?: number;
+  penaltyPoints?: number;
+  rewardPoints?: number;
+  totalPoints?: number;
+  hasCourt?: boolean;
+}
