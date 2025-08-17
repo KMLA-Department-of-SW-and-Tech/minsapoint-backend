50e5a79727ab80df17d70d28b61c69d49c1ab673
codeweb404 <codeweb.jcu@gmail.com>
2025-08-17 23:39:59 +0900

    created me service

--- src/services/me.service.ts
+++ src/services/me.service.ts
@@ -5,11 +5,36 @@ import { UserRole } from '../schemas/models';
 
 @Injectable()
 export class MeService {
-  async getMe(user): Promise<UserResponseDto> {
-    return;
+  private readonly usersCollection = db.collection('users');
+
+  async getMe(user: any): Promise<UserResponseDto> {
+    const snapshot = await this.usersCollection.where('firebaseUID', '==', user.uid).get();
+    if(!snapshot.empty) {
+        throw new NotFoundException(`User not found`);
+    }
+    return {
+        _id: snapshot.docs[0].id,
+        ...snapshot.docs[0].data(),
+    } as UserResponseDto;
   }
 
-  async updateMe(user, updateUserDto): Promise<UserResponseDto> {
-    return;
+  async updateMe(user:any, updateUserDto: UpdateUserDto): Promise<UserResponseDto> {
+    const snapshot = await this.usersCollection.where('firebaseUID', '==', user.uid).limit(1).get();
+    if(!snapshot.empty) {
+        throw new NotFoundException(`User not found`);
+    }
+
+    const updateData = {
+        studentNumber: updateUserDto.studentNumber === undefined ? snapshot.docs[0].data().studentNumber : updateUserDto.studentNumber,
+        name: updateUserDto.name === undefined ? snapshot.docs[0].data().name : updateUserDto.name,
+    }
+
+    const ref = this.usersCollection.doc(snapshot.docs[0].id);
+    await snapshot.docs[0].ref.update(updateData);
+    
+    return {
+        ...snapshot.docs[0].data(),
+        ...updateData,
+    } as UserResponseDto;
   } 
 }
